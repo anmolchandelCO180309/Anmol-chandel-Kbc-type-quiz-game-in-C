0ce0648db07650af61494198cc3e3da8a28004ce
anmolchandelCO180309 <42858993+anmolchandelCO180309@users.noreply.github.com>
2018-09-02 19:21:47 +0530

    Add files via upload

---
+++ kbc.c
@@ -0,0 +1,239 @@
+#include<stdio.h>
+void main()
+{
+char name[100];
+char ch;
+int ans,cnt=0;
+clrscr();
+printf("welcome to KBC mr/ms:");
+puts(name);
+printf("RULES FOR KBC");
+printf("1.THE GAME CONSISTS OF 10 QUESTIONS");
+printf("2.EVERY QUESTIONS CONSIST OF 4 OPTIONS");
+printf("FOR EVERY CORRECT OPTION U WILL GET 1000 RS");
+printf("U WILL BE EXIST FOR WRONG OPTION");
+printf("YOU CAN USE 50-50 OPTION ONLY ONE TIME");
+printf("YOU CAN ONLY USE 50-50 ONLY AFTER 8 QUESTION");
+printf("IF YOU ARE INTERESTED TO PLAY THE GAME PRESS enter TO ENTER INTO GAME");
+ch=getch();
+printf("HERE WE GO..");
+printf("THE GAME STARTS NOW\n...BEST OF LUCK\n..GIVE UR BEST\n...);
+puts(name);
+printf("Q1.WHO IS THE PRESIDENT OF INDIA?\n");
+printf("1.RAMNATH KOVIND\t 2.NARENDER MODI\t 3.AMIT SHAH\t 4.LALU PRASAD YADAV\n");
+printf("PLEASE INPUT THE OPTION NUMBER!\n");
+scanf("%d", &opt);
+if(opt=1)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 1000rs");
+}
+else
+{
+printf("YOU LOOSE")
+exit(0);
+}
+printf("Q2.WHO IS THE FIRST WOMEN TO SUCCESSFULLY CLIMB K2,THE WORLD'S SECOND HIGHEST MOUNTAIN PEAK?\n");
+printf("1.JUNKO TABEI\t 2.WANDA RUTKIEWICZ\t 3.TAMAE WATANABE\t 4.CHANTAK MAUDITA\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==2)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 2000 rs");
+}
+else
+{
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q3.WHO,IN 1978,BECAME THE FIRST PERSON TO BE BORN IN THE CONTINENT OF ANTARTICA?\n");
+printf("1.EMILIO PALMA\t 2.JAMES WEDDEL\t 3.JOMES\t 4.CHARLES WILKES\n");
+printf("PLEASE INPUT OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==1)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 3000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q3.WHO COMMANDED THE 'HECTOR',THE FIRST BRITISH TRADING SHIP TO LAND SURAT?\n");
+printf("1.JETHALAL\t 2.SWILLIAM\t 3.TOM CRUISE\t 4.TOM HOLLAND\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==2)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 4000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q5.WHAT WAS THE NAME GIVEN TO THE SUPERCLUSTER GALAXIES,DISCOVERED BY ATEAM OF INDIAN ASTRONOMERS IN 2017?\n")
+printf("1.TOM HOLLAND\t 2.CHRIS EVANS\t 3.TONY STARK\t 4.SARSWATI\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==4)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 5000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q6.WHICH COLONIAL POWER ENDED ITS INVOVLEMENT IN INDIA BY SELLING THE RIGHTS OF NICOBER ISLANDS TO THE BRITISH ON OCTOBER18,1868?\n");
+printf("1.GOKULDHAM SOCIETY\t 2.DENMARK\t 3.GURGAON\t 4.NAGPUR\n");
+printf("PLEASE INPUT YOUR OPTION NUMBER");
+scanf("%d",&opt);
+if(opt==2)
+{
+printf("YOUR ANSWER IS CORRECT");
+printf("YOU WON 6000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q7.WHICH OF THE FORT WAS NOT BUILT BY EUROPEON COLONIAL POWERS IN INDIA\n");
+printf("1.GURG FORT\t 2.FORT CHAMBREY\t 3.GOLCONDA\t 4.RED FORT\n");
+scanf("%d",&opt);
+if(opt==2)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 7000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q8.AMONG WHOM DOES THE INDIAN CONSTITUTION PERMIT TO TAKE PART IN THE PROCEEDINGS OF PARLIAMENT?\n");
+printf("1.ATORNEY GENERAL\t 2.JACKIE CHAN\t 3.TOM CRUISE\t 4.VIN DIESEL\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n")
+scanf("%d",&opt);
+if(opt==1)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 8000 rs");
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q9.WHO PLAYED NEERJA ROLE IN MOVIE OF NEERJA?\n");
+printf("1.RAKHI SAWANT\t 2.MONISHA\t 3.KATRINA\t 4.SONAM\n")
+printf("INPUT YOUR YOUR OPTION NUMBER?\n")
+scanf("%d",&opt);
+if(opt==4)
+{
+printf("YOUR ANSWER IS CORRECT");
+printf("YOU WON 9000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q10.WHICH OF THESE CITIES LOCATED IN THE STATE OF GUJARAT IS FAMOUS FOR ZARI PRODUCTION?\n");
+printf("1.SURAT\t 2.U.S.A\t 3.ARGENTINA\t 4.INDONESIA\n");
+printf("PLEASE INPUT THE OPTION NUMBER"\n);
+scanf("%d",&opt);
+if(opt==1)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 10,000 rs");
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q11.NAME THE INDIAN TENNIS PLAYER WHO TURNED INTO BOLLYWOOD FILMMAKER?\n");
+printf("1.RICKY POINTING\t 2.SACHIN TENDULKAR\t 3.ASHOK AMRITRAJ\t 4.YUVRAJ\n");
+printf("PLEASE INPUT YPUR OPTION NUMBER"\n);
+scanf("%d",&opt);
+if(opt==3)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 11,000 rs");
+}
+else
+printf("YOU LOOOSE");
+exit(0);
+}
+printf("Q12.WHO ACTED AS NATASHA ROMANOFF IN IRON MAN2\n");
+printf("1.AMELLA TROY\t 2.SCARLETT JOHANSON\t 3.GAL GODAT\t 4.EMMA WATSON\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+{
+if(opt==2)
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 12,000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q13.WHO WAS THE LEAD ACTOR IN THE MOVIE RAMPAGE?\n");
+printf("1.CHRISTOPHER\t 2.VAN PERSIE\t 3.VIN DIESEL\t 4.DWAYNE JOHANSON\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==4)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 13,000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q14.WHO WAS THE DEPUTY CHIEF MINSTER OF MAHARSTRA IN 2012\n");
+printf("1.ARUN JAITLEY\t 2.KIRON KHER\t 3.AJIT PANWAR\t 4.AJIT PAWAR\n");
+printf("PLEASE INPUT THE OPTION NUMBER\n");
+scanf("%d",&opt);
+if(opt==4)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 14,000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("Q15.AMONG WHOM DOES THE INDIAN CONSTITUION PERMIT TO TAKE PART IN PROCEEDINGS OF PARLIAMENT\n")
+printf("1.SOLOCTER GENERAL\t 2.ATTOTNEY GENERAL\t 3.CABINET SECRETORY\t 4.CHIEF JUSTICE\n");
+printf("PLEASE INPUT THE OPTION NUMBER"\n);
+scanf("%d",&opt);
+if(opt==2)
+{
+printf("YOUR ANSWER IS CORRECT\n");
+printf("YOU WON 15,000 rs");
+}
+else
+printf("YOU LOOSE");
+exit(0);
+}
+printf("\t\tCONGRATULATIONS YOU HAVE ATTEMPTED ALL QUESTIONS RIGHT\n");
+printf("\n\t\tyou won 15,000rs\n");
+printf("\t\t YOU ARE THE NEW WINNER OF THIS GAME\n");
+printf("YOU ARE A BILLIONAIRE!!!!!!");
+}
+else
+{
+printf("OOPS!!YOUR ANSWER IS WRONG\n");
+printf("\n YOU WON NOTHING");
+}
+
+
+}
+
+
+
+
+
+
+
+
+ 
+
+
